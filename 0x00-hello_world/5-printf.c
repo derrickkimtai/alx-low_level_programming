38aa75acc477be17399fa977206e671c000401ba
derrickkimtai <derrickmoio92@gmail.com>
2023-03-16 07:04:48 -0700

    financing

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
  * main - starting point 
  * Return : endpoint 0 succes
  **/
-	int main(void) 
-{		
- printf ("with proper grammar, but the outcome is a piece of art\n");
+	int main(void)
+:{		
+ printf("with proper grammar, but the outcome is a piece of art\n");
 return (0);
 }	
 		 
