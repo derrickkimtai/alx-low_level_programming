dfd1945a9c0a853d987c4fa22a20deee89a3d003
derrickkimtai <derrickmoio92@gmail.com>
2023-03-16 07:20:38 -0700

    north

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main- starting point 
- * Return: endpoint 0 succes
+ *main- starting point 
+ *Return: endpoint 0 succes
  */
 int main(void)
-{		
+{	
 printf("with proper grammar, but the outcome is a piece of art\n");
 return (0);
-}	
+}
 		 
