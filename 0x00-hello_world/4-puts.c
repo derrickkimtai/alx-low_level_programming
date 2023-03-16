59eba6d2d2383ce7d3449eb1b3904b0728d86fe8
derrickkimtai <derrickmoio92@gmail.com>
2023-03-16 06:34:19 -0700

    with

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h> 
+
 /** 
- *main_entry point
- *return-succes
+ *main - Entry point
+ *Return: Always 0 (success)
  **/
-int main (void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-
 }
  
