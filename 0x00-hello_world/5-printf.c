201d03a83fcc1df6e9ac92dcfa30cd700efa10be
derrickkimtai <derrickmoio92@gmail.com>
2023-03-16 07:00:16 -0700

    shift

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
  * main - starting point 
  * Return : endpoint 0 succes
  **/
-{
-	int main(void)
+	int main(void) 
+{		
  printf ("with proper grammar, but the outcome is a piece of art\n");
 return (0);
 }	
