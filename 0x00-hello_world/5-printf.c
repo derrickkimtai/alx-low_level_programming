4ea3d2c55ca5e5bc64375b53ab24f5531a2909a4
derrickkimtai <derrickmoio92@gmail.com>
2023-03-16 07:07:54 -0700

    policies

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - starting point 
  * Return : endpoint 0 succes
- **/
-	int main(void)
-:{		
- printf("with proper grammar, but the outcome is a piece of art\n");
+ */
+int main(void)
+{		
+printf("with proper grammar, but the outcome is a piece of art\n");
 return (0);
 }	
 		 
