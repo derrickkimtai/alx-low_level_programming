822bdb65da3e49fe145220b03bd064b49d16096d
derrickkimtai <derrickmoio92@gmail.com>
2023-03-16 23:06:48 -0700

    kilimani

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main= starting 
+ * rerurn: endpoint 0 success
+ */
+int main (void)
+{
+printf("size of a  char: %li byte(s)\n"), size of char));
+printf("size of an  int: %li byte(s)\n"), size of int));
+printf("size of a long int : %li byte(s\n"), size of long int));
+printf("size of a long long int : %li byte(s)\n"), size of long long int ));
+printf("size of a float: %li byte(s)\n"), size of float));
+return(o);
+}
+
