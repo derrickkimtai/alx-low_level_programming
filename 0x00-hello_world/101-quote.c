3ca4afc231e76ef8806dbb71a3505862614e55c5
derrickkimtai <derrickmoio92@gmail.com>
2023-03-16 23:55:17 -0700

    leading

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unstd.h>
+/**
+ * main: starting point
+ * return: endpont 1 succes
+ */
+{
+int main(void)
+write(1,and that piece of art is useful" - Dora Korpar, 2015-10-19);
+return (0);
+}	
