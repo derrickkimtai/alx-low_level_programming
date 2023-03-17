4816c59138aad72078346025c12f3aa4b6a3a318
derrickkimtai <derrickmoio92@gmail.com>
2023-03-16 23:19:36 -0700

    kilimani

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,11 @@
  */
 int main (void)
 {
-printf("size of a  char: %li byte(s)\n"), size of char));
-printf("size of an  int: %li byte(s)\n"), size of int));
-printf("size of a long int : %li byte(s\n"), size of long int));
-printf("size of a long long int : %li byte(s)\n"), size of long long int ));
-printf("size of a float: %li byte(s)\n"), size of float));
+printf("Size of a  char: %li byte(s)\n"), sizeof(char));
+printf("Size of an  int: %li byte(s)\n"), sizeof(int));
+printf("Size of a long int : %li byte(s\n"), sizeof(long int));
+printf("Size of a long long int : %li byte(s)\n"), sizeof(long long int ));
+printf("Size of a float: %li byte(s)\n"), sizeof(float));
 return(o);
 }
 
