d49facdb830d68086904b7ac7a21094fc1f3e3ab
derrickkimtai <derrickmoio92@gmail.com>
2023-03-17 02:52:03 -0700

    uptake

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main- Entrypoint
- * return- (1)  succes
+ * main - Entry point
+ * Return:1
  */
-int main (void)
+int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
