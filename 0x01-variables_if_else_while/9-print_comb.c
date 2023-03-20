01ca1d0510c666e487515d2888c4f41b6188adf7
derrickkimtai <derrickmoio92@gmail.com>
2023-03-20 09:41:27 -0700

    number 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,9 +9,14 @@ int main(void)
 int i;
 for (i = 0; i < 10 ; i++)
 {
+	if (i ==9)
+	putchar (i + '0');
+	else
+	{	
 	putchar(i + '0');
 	putchar(',');
 	putchar(' ');
+	}
 }
 putchar('\n');
 return (0);
