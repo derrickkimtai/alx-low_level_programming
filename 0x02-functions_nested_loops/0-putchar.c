fc57b207e94f179885a7acd0df900f1cbdbbe6f9
derrickkimtai <derrickmoio92@gmail.com>
2023-03-21 06:00:41 -0700

    task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - main function
- *
- * Return: always 0
+ * _putchar: print the alphabet putchar
+ * Return:always 0
  */
 int main(void)
 {
-printf("_putchar");
-printf("\n");
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
