24d83abcc42055279506037afdce50a5fbe00762
derrickkimtai <derrickmoio92@gmail.com>
2023-03-24 08:43:52 -0700

    task 6

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,9 +10,9 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for  (j = 0; j < 15; j++)
+		for (j = 0; j < 15; j++)
 		{
-			 if (j >= 10)
+			if (j >= 10)
 				_putchar(j / 10 + '0');
 			_putchar(j % 10 + '0');
 		}
