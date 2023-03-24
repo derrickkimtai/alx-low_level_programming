2747dbdeaf33ec7f4e76a811129984cfb45ed588
derrickkimtai <derrickmoio92@gmail.com>
2023-03-24 08:01:00 -0700

    task 10

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,9 +12,9 @@ int main(void)
 	{
 		if (i % 15 == 0)
 			printf("FizzBuzz");
-		else if(i % 3 == 0)
+		else if (i % 3 == 0)
 			printf("Fizz");
-		else if(i % 5 == 0)
+		else if (i % 5 == 0)
 			printf("Buzz");
 		else
 			printf("%d", i);
