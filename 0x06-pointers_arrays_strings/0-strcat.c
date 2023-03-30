d820895e5a727deb7adefc198a6efc772761f3c5
derrickkimtai <derrickmoio92@gmail.com>
2023-03-30 00:52:53 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_strcat - concatinate two strings
+ *@dest: destination
+ *@src: source
+ *Return: destination string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int destlen = 0;
+	int srclen = 0;
+	int i;
+
+	for (i = 0; dest[i] != '\0' ; i++)
+		destlen++;
+	for (i = 0 ; src[i] != '\0' ; i++)
+		srclen++;
+	for (i = 0 ; i <= srclen ; i++)
+		dest[destlen + 1] = src[i];
+	return (dest);
+}
