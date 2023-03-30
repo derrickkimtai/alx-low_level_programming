8c711407c46642989a10065d83f4ade857942ff1
derrickkimtai <derrickmoio92@gmail.com>
2023-03-30 03:16:26 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *_strcmp - compares two string
+ *@s1: string 1
+ *@s2: string 2
+ *Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0 ; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			if (s1[i] < s2[i])
+				return (s1[i] - s2[i]);
+			else if (s1[i] > s2[i])
+				return (s1[i] - s2[i]);
+		}
+		else
+			return (0);
+	}
+	return (0);
+}
