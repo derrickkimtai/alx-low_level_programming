21f9826a12ffe05ddfa36022d10b54b97bd557ab
derrickkimtai <derrickmoio92@gmail.com>
2023-04-11 19:07:44 +0000

    task 1234

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+int _putchar(char c)
+
+
+#endif
