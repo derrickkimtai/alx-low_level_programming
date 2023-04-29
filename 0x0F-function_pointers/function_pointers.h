442b64f0dd62fecb13d0a865a5437aab88b6ccdf
derrickkimtai <derrickmoio92@gmail.com>
2023-04-29 11:12:06 +0000

    last chance

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,28 @@
-#ifndef MAIN_H
-#define MAIN_H
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
-int (*get_op_func(char *s))(int, int);
-int _putchar(char c);
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+/**
+ * struct p - checker struct
+ * @p: possibility
+ * @f: Function to be used
+ */
+typedef struct p
+{
+  char *p;
+  void (*f)(va_list);
+} checker;
+
+void print_c(va_list);
+void print_i(va_list);
+void print_f(va_list);
+void print_s(va_list);
 
 #endif
+
