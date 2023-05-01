7ac12d182f1e061c58dbaad30bcde306f68d5d87
derrickkimtai <derrickmoio92@gmail.com>
2023-05-01 10:15:57 +0000

    sikia

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,9 +2,8 @@
 
 /**
  * print_listint - prints all the elements of a linked list
- * @h: linked list of type listint_t to print
- *
- * Return: number of nodes
+ * @h: pointer
+ * Return: number of integers
  */
 size_t print_listint(const listint_t *h)
 {
