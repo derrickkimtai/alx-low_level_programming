96dc7af19b09310037ec48b9591a66e53b1b45aa
derrickkimtai <derrickmoio92@gmail.com>
2023-05-01 12:27:25 +0000

    commintment

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,28 +2,26 @@
 
 /**
  * main - Entry point
- *
- * Return: Always 0 (Succes)
+ * Return:  0 (Success)
  */
-
 int main(void)
 {
-	int n;
-	int i = 0;
+	int n, m;
 
-	for (n = 0 ; n < 100 ; n++)
+	for (n = 48; n <= 56; n++)
 	{
-		putchar(n / 10 + '0');
-		putchar(i + '0');
-		if (n < 99)
+		for (m = 49; m <= 57; m++)
 		{
-			putchar(',');
-			putchar(32);
-		}
-		i++;
-		if (i > 9)
-		{
-			i = 0;
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
