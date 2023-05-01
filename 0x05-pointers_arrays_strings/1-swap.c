9c8a37dd5e0562553cbbd7f8f600bb3b9c31469e
derrickkimtai <derrickmoio92@gmail.com>
2023-05-01 14:05:54 +0000

    back up

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,27 +1,17 @@
 #include "main.h"
 /**
-<<<<<<< HEAD
- *swap_int - interchanges the value of the integers
- *@a: swaps and store addresess of b
- *@b: swaps and stores addreses of a
-=======
- *swap_init - take two integers and interchange
- *@a: swaps and store address a
- *b: swaps and stores address of b
->>>>>>> 8b52395598657d4a54c7f70e838965a918c17fce
- *Return: 0
+ * swap_int - swaps the value of int a and int b
+ * [Prince Solomon]
+ * @a:  int
+ * @b: int
+ * Return 0
  */
+
 void swap_int(int *a, int *b)
 {
-<<<<<<< HEAD
-int change;
-change = *b;
-*b = *a;
-*a = change;
-=======
-int c = *a;
+	int s;
 
+	s = *a;
 	*a = *b;
-	*b = c;
->>>>>>> 8b52395598657d4a54c7f70e838965a918c17fce
+	*b = s;
 }
