d1be3e9e2d05a969ebd4a3c2207423dfb63a33a3
derrickkimtai <derrickmoio92@gmail.com>
2023-06-07 08:23:58 +0000

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,12 +5,11 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+
+	if (*s != '\0')
 	{
-		_putchar('\n');
-		return;
-	}
 	_print_rev_recursion(s + 1);
 	_putchar(*s);
-
+	}
+	_putchar('\n');
 }
