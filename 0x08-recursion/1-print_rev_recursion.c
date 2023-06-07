fa66806912a0998049b3bd83cd1d48c344fa2a7c
derrickkimtai <derrickmoio92@gmail.com>
2023-06-07 08:14:39 +0000

    recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_print_rev_recursion -  prints astring in reverse
+ *@s: string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+
+}
