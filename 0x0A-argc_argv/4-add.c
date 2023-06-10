d4bf8d9556bfe0163549e4b375a6bfe9d1a5b4bc
derrickkimtai <derrickmoio92@gmail.com>
2023-06-10 09:25:09 +0000

    command line argument

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ *
+ *
+ */
+int main(int argc, char **argv[])
+{
+	int num1, num2, result;
+
+	if (argc = {} )
+	{
+		printf("0\n");
+	}
+	else if (argc != isdigit)
+			printf("Error\n");
+			return 1;
+
+			num1 = atoi(argv[1]);
+			num2 = atoi(argv[2]);
+			result = num1 + num2;
+			printf("%d\n", result);
+			return (0);
+
+
+
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 int _putchar(char c);
 int main(int argc char **argv[]);
-int multiplyNum(int, int);
 #include <stdio.h>
 #include <stdlib.h>
 #endif
