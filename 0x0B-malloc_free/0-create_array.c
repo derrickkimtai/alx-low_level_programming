99fce57c006bc09f8300b730c96b4c3f65074c25
derrickkimtai <derrickmoio92@gmail.com>
2023-06-14 21:26:30 +0000

    memmory allocation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,10 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 	 array = (char *) malloc(size * sizeof(char));
-
+	if (array == NULL)
+	{
+			return (NULL);
+	}
 		for (i = 0; i < size; i++)
 		{
 			array[i] = c;
