52d83e17b4684555c31dbdaa05db83331ec9b6da
derrickkimtai <derrickmoio92@gmail.com>
2023-06-16 12:56:02 +0000

    memmory allocation

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,6 +9,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;
+	size_t copy_size;
 
 	if (new_size == 0)
 	{
@@ -28,7 +29,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		return (NULL);
 	}
-	memcpy(new_ptr, ptr, old_size);
+	copy_size = (old_size < new_size) ? old_size : new_size;
+	memcpy(new_ptr, ptr, copy_size);
 	free(ptr);
 	return (new_ptr);
 }
