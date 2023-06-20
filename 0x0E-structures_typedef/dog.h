3702fb6461c2982e371d86c527fdf8276e56ad14
derrickkimtai <derrickmoio92@gmail.com>
2023-06-20 07:44:35 +0000

    structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - structures rep dogs
+ *@name : name of the dog
+ *@age : age of the dog
+ *@owner : owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
