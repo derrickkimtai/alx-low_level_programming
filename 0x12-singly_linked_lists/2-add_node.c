44b5ba256eceb31d7ab29c1d6eb02cfce039f619
derrickkimtai <derrickmoio92@gmail.com>
2023-06-30 13:12:55 +0000

    linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,7 +9,12 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	unsigned int len = 0;
 
+	while (str[len])
+	{
+		len++;
+	}
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
