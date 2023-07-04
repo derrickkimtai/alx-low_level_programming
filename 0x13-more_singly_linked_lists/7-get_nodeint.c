f878cce756225ff196bd5b3198b8a1975fefc054
derrickkimtai <derrickmoio92@gmail.com>
2023-07-04 08:26:08 +0000

    linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,7 +14,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		return (NULL);
 	}
-	while (i < index)
+	while (node != NULL && i < index)
 	{
 		node = node->next;
 		i++;
