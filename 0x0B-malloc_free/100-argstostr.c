4101fa635e16a6897ad2e2848732500efcc94149
derrickkimtai <derrickmoio92@gmail.com>
2023-07-24 09:51:19 +0000

    shell

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ *argstostr - concatinates array of string
+ *@ac: arguments count
+ *@av: argument vector
+ *Return: string
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j;
+	int len = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		len += strlen(av[i]) + 1;
+	}
+	str = (char *) malloc(sizeof(char) * len);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[len++] = av[i][j];
+		}
+		str[len++] = '\n';
+	}
+	return (str);
+}
